2503639eab7a40f277fae64002f7aafeb324f489
Nils Maier <maierman@web.de>
2011-01-08 23:06:08 +0100

    Python < 2.7 compat

--- build.py
+++ build.py
@@ -23,7 +23,15 @@ if os.path.exists(destination):
     print >>sys.stderr, destination, "is in the way"
     sys.exit(1)
 
-with ZipFile(destination, "w", ZIP_DEFLATED) as zp:
+class ZipOutFile(ZipFile):
+    def __init__(self, zfile):
+        ZipFile.__init__(self, zfile, "w", ZIP_DEFLATED)
+    def __enter__(self):
+        return self
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+with ZipOutFile(destination) as zp:
     for f in sorted(get_files(resources), key=str.lower):
         if f.endswith('.png'):
             zp.write(f, compress_type=ZIP_STORED)
